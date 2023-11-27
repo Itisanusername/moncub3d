9a79eea15a83ea3e497f4050e79db51c7ee3d27e
q <julien.masto@gmail.com>
2023-11-27 01:03:13 +0100

    o

--- objs/extract.d
+++
@@ -1,8 +0,0 @@
-objs/extract.o: srcs/extract.c includes/cub3d.h \
- includes/../minilibx-linux/mlx.h libft/libft.h libft/ft_printf.h \
- libft/get_next_line.h
-includes/cub3d.h:
-includes/../minilibx-linux/mlx.h:
-libft/libft.h:
-libft/ft_printf.h:
-libft/get_next_line.h:
